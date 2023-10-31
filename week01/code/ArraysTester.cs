517c86cece08cf938a2e5976fe52d7d4d94daa89
Bekiboo <julien.connault@gmail.com>
2023-10-31 16:21:53 +0300

    week 1 - code

--- week01/code/ArraysTester.cs
+++ week01/code/ArraysTester.cs
@@ -1,8 +1,10 @@
-public static class ArraysTester {
+public static class ArraysTester
+{
     /// <summary>
     /// Entry point for the tests
     /// </summary>
-    public static void Run() {
+    public static void Run()
+    {
         // Sample Test Cases (may not be comprehensive)
         Console.WriteLine("\n=========== PROBLEM 1 TESTS ===========");
         double[] multiples = MultiplesOf(7, 5);
@@ -34,14 +36,19 @@ public static class ArraysTester {
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     private static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // We need to create a list to hold the results, then we'll loop through the length 
+        // and add the number multiplied by the index to the list.  Finally, we'll return the list
+        // as an array.
+        List<double> results = new();
 
-        return new double[0]; // replace this return statement with your own
+        for (int i = 0; i < length; i++)
+        {
+            results.Add(number * (i + 1));
+        }
+
+        return results.ToArray();
     }
-    
+
     /// <summary>
     /// Rotate the 'data' to the right by the 'amount'.  For example, if the data is 
     /// <c>&lt;List&gt;{1, 2, 3, 4, 5, 6, 7, 8, 9}</c> and an amount is 3 then the list returned should be 
@@ -52,10 +59,11 @@ public static class ArraysTester {
     /// </summary>
     private static void RotateListRight(List<int> data, int amount)
     {
-        // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
-
+        // We need to remove the last 'amount' items from the list and then add them to the beginning of the list.
+        // We can use the GetRange method to get the last 'amount' items and then use the RemoveRange method to remove
+        // them from the list.  Finally, we can use the InsertRange method to add them to the beginning of the list.
+        var sublist = data.GetRange(data.Count - amount, amount);
+        data.RemoveRange(data.Count - amount, amount);
+        data.InsertRange(0, sublist);
     }
 }
